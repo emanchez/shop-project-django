7ab20655acec0c72937958762b3ffb95012e861e
emanchez <sanchez.emmanuelm@gmail.com>
2025-05-30 21:14:02 -0400

    nah

--- api/index.py
+++ api/index.py
@@ -1,10 +1,12 @@
-from serverless_wsgi import handle_request
-from django.core.wsgi import get_wsgi_application
 import environ
+from django.core.wsgi import get_wsgi_application
+from serverless_wsgi import handle_request
 
 environ.setdefault("DJANGO_SETTINGS_MODULE", "shop1django.settings")
 
+# Initialize Django application
 django_app = get_wsgi_application()
 
+# Vercel-required handler function
 def handler(event, context):
-    return handle_request(django_app, event, context)  # Must use this exact signature
+    return handle_request(django_app, event, context)
