f5fa1a98e5c388e5dcd39403b31864372dbdfb94
emanchez <sanchez.emmanuelm@gmail.com>
2025-05-30 20:19:08 -0400

    configure deployment

--- shop1django/settings.py
+++ shop1django/settings.py
@@ -31,8 +31,12 @@ SECRET_KEY = env('DJANGO_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
-
+ALLOWED_HOSTS = [
+    '.vercel.app',
+    '.now.sh',
+    'localhost',
+    '127.0.0.1'
+]
 
 # Supabase PostgreSQL Database
 DATABASES = {
